2c93afad4ffae0474ab37f920828be30d76ff06c
Vincent CHAPRON <vincent@chapron.io>
2017-09-26 11:42:31 +0200

    Separator must be '_'

--- src/Provider/SlugProvider.php
+++ src/Provider/SlugProvider.php
@@ -4,7 +4,7 @@ namespace App\Provider;
 
 class SlugProvider {
 
-    public function slugify($name, $separator = '-') {
+    public function slugify($name, $separator = '_') {
         $slug = preg_replace('/[^\w]+/', $separator, $name);
         $slug = strtolower(trim($slug, $separator));
         return $slug;
