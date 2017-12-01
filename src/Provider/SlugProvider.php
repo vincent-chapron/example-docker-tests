af0c0c2ae928e4d186c569a3845cc8de724e871d
Vincent CHAPRON <vincent@chapron.io>
2017-12-01 16:28:52 +0100

    add optionnal parameter

--- src/Provider/SlugProvider.php
+++ src/Provider/SlugProvider.php
@@ -4,7 +4,7 @@ namespace App\Provider;
 
 class SlugProvider {
 
-    public function slugify($name, $separator = '-', $prefix) {
+    public function slugify($name, $separator = '-', $prefix = '') {
         $slug = preg_replace('/[^\w]+/', $separator, $name);
         $slug = strtolower(trim($slug, $separator));
         return $prefix . $separator . $slug;
