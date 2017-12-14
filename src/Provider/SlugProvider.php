b6d489b95bb46b06ca3a6b12f9469d95850b8fa7
Vincent CHAPRON <vincent@chapron.io>
2017-12-14 15:50:45 +0100

    fix separator

--- src/Provider/SlugProvider.php
+++ src/Provider/SlugProvider.php
@@ -7,7 +7,7 @@ class SlugProvider {
     public function slugify($name, $separator = '-', $prefix = '') {
         $slug = preg_replace('/[^\w]+/', $separator, $name);
         $slug = strtolower(trim($slug, $separator));
-        return $prefix . $separator . $slug;
+        return ($prefix) ? $prefix . $separator . $slug : $slug;
     }
 
 }
